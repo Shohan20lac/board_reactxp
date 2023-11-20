0b548d26e179913310324a1e3d40065f7faa7bc8
shohan20lac <r.shohan12@gmail.com>
2023-11-20 09:07:51 +0600

    Project and Homepage initialized, bottomnav implemented
    ready for incrementatl feature addition

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+
+const actions = [
+  {
+    text: "New Task",
+    icon: require("./assets/digicurr.png"),
+    name: "bt_language",
+    position: 1
+  },
+  {
+    text: "Location",
+    icon: require("./assets/favicon.png"),
+    name: "bt_room",
+    position: 3
+  }
+];
+
+function NotesScreen () {
+  const [notes, setNotes] = useState([])
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.routeContainer}>
+      <Text> </Text>
     </View>
   );
 }
 
+function TasksScreen () {
+  const [tasks, setTasks] = useState([])
+  
+  return (
+    <View style={styles.routeContainer}>
+      <Text> </Text>
+    </View>
+  );
+}
+
+function WalletScreen () {
+  const [wallet, setWallet] = useState([])
+  
+  return (
+    <View style={styles.routeContainer}>
+      <Text> </Text>
+    </View>
+  );
+}
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={styles.tabBar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.tabItem}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+        <Tab.Screen name="Notes" component={NotesScreen} />
+        <Tab.Screen name="Tasks" component={TasksScreen} />
+        <Tab.Screen name="Wallet" component={WalletScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  tabBar: {
+    flexDirection: 'row',
+  },
+  tabItem: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tabBarText: {
+    color: '#222',
+  },
+  tabBarTextFocused: {
+    color: '#673ab7',
+  },
+  routeContainer: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  }
 });
